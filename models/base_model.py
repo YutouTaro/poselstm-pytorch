d123267ff801dbeff710dc08ec9be2be4c5bbb3c
YutouTaro <kxhyu17@gmail.com>
2020-04-21 19:35:16 +0800

    Update base_model.py
    update network parameter saving format

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,7 @@ class BaseModel():
 
     # helper saving function that can be used by subclasses
     def save_network(self, network, network_label, epoch_label, gpu_ids):
-        save_filename = '%03d_net_%s.pth' % (epoch_label, network_label)
+        save_filename = '%03d_net_%s.pth' % (int(epoch_label), network_label)
         save_path = os.path.join(self.save_dir, save_filename)
         torch.save(network.cpu().state_dict(), save_path)
         if len(gpu_ids) and torch.cuda.is_available():
