a7808fc58906e175f140f09eaa435105816c26fb
yu <kxhyu17@gmail.com>
2020-10-30 13:30:49 +0800

    building architecture for fcnlstm

--- options/train_options.py
+++ options/train_options.py
@@ -24,5 +24,6 @@ class TrainOptions(BaseOptions):
         self.parser.add_argument('--lr_policy', type=str, default='lambda', help='learning rate policy: lambda|step|plateau')
         self.parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
         self.parser.add_argument('--init_weights', type=str, default='pretrained_models/places-googlenet.pickle', help='initiliaze network from, e.g., pretrained_models/places-googlenet.pickle')
+        self.parser.add_argument('--pretrained_model', type=str, help='pretrained FCN model')
 
         self.isTrain = True
