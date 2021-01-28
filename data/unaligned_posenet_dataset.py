8ffa193b644ab2c3d22b7694a4760e84b129645e
YutouTaro <kxhyu17@gmail.com>
2021-01-28 13:36:36 +0800

    Update unaligned_posenet_dataset.py

--- data/unaligned_posenet_dataset.py
+++ data/unaligned_posenet_dataset.py
@@ -23,7 +23,7 @@ class UnalignedPoseNetDataset(BaseDataset):
             # TODO find a better way to store position_range
             file_name = os.path.join(self.opt.checkpoints_dir, self.opt.name, 'opt_'+self.opt.phase+'.txt')
             with open(file_name, 'at') as opt_file:
-                opt_file.write('position_range: {}\n'.format(opt.position_range[0], opt.position_range[1]))
+                opt_file.write('position_range: {}, {}\n'.format(opt.position_range[0], opt.position_range[1]))
         else:
             # read the position_range used for training from opt_train.txt
             path_train_file = os.path.join(opt.checkpoints_dir, opt.name, 'opt_train.txt')
